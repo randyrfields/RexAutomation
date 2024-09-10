9516bc89f60e48dd47e51b02223da547faec1fe8
Randy Fields <rfields@rexairllc.com>
2024-09-10 15:32:59 -0400

    add allowed host

--- rexauto/rexauto/settings.py
+++ rexauto/rexauto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6s3-8khc)*19eehf^hs-flmlm67wi))jklrd)x&0y@2szw%ei#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.253"]
 
 
 # Application definition
